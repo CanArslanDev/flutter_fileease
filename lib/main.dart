2d43b4f94919cd9061c8b7859ce553f3db71620a
Can Arslan <canarslanx@gmail.com>
2024-01-08 00:46:45 +0300

    modified:   ios/Runner.xcodeproj/project.pbxproj 	modified:   ios/Runner/Info.plist 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,16 @@ import 'package:responsive_sizer/responsive_sizer.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyD3Zr7yMGM4FUwZp_5KtbC35w-712DLPZg',
-      authDomain: 'flutter-fileease.firebaseapp.com',
-      projectId: 'flutter-fileease',
-      storageBucket: 'flutter-fileease.appspot.com',
-      messagingSenderId: '239466517804',
-      appId: '1:239466517804:web:a58d6d36cbca8e8fd04889',
-    ),
+    options: (!WebService.isWeb)
+        ? null
+        : const FirebaseOptions(
+            apiKey: 'AIzaSyD3Zr7yMGM4FUwZp_5KtbC35w-712DLPZg',
+            authDomain: 'flutter-fileease.firebaseapp.com',
+            projectId: 'flutter-fileease',
+            storageBucket: 'flutter-fileease.appspot.com',
+            messagingSenderId: '239466517804',
+            appId: '1:239466517804:web:a58d6d36cbca8e8fd04889',
+          ),
   );
   runApp(const MyApp());
 }
